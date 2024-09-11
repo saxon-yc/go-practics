34a4445c51f5aa24ec2707332c7218c615d07831
sishenghuang <sishenghuang@yunify.com>
2024-09-11 17:48:49 +0800

    feat: 文件是否存在

--- internal/syntax/t_chart.go
+++ internal/syntax/t_chart.go
@@ -1,7 +1,9 @@
 package syntax
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 )
 
 type ChartArgs struct {
@@ -9,6 +11,7 @@ type ChartArgs struct {
 	ChartName    string
 	ChartVersion string
 	NameSpace    string
+	RegistryURL  string
 	Values       string
 	RepoName     string
 	RepoURL      string
@@ -25,6 +28,8 @@ func UpdateAddonChart() (err error) {
 	// var chart ChartArgs
 	chart := new(ChartArgs)
 	chart.initChartValues()
+	ok, err := chart.isLogined()
+	fmt.Printf("ok: %v,err: %v\n", ok, err)
 
 	resultValues := oldValues
 	if err = chart.joinURL(&resultValues, "config", "image", "value"); err != nil {
@@ -39,6 +44,7 @@ func (chart *ChartArgs) initChartValues() error {
 	chart.NameSpace = "default"
 	chart.Values = ``
 	chart.RepoName = "my-repo"
+	chart.RegistryURL = "192.168.2.244:8080"
 	return nil
 }
 func (chart *ChartArgs) joinURL(resultValues *ChartValues, configName, valueName string, value interface{}) error {
@@ -50,4 +56,37 @@ func (chart *ChartArgs) joinURL(resultValues *ChartValues, configName, valueName
 		return nil
 	}
 	return fmt.Errorf("JoinURL invalid value %v, should be a string", value)
+}
+
+type AuthConfig struct {
+	Auths map[string]struct {
+		Auth string `json:"auth"`
+	} `json:"auths"`
+}
+
+func (chart *ChartArgs) isLogined() (bool, error) {
+	configFilePath := fmt.Sprintf("%s/config.json", os.Getenv("HOME"))
+	var (
+		file []byte
+		data AuthConfig
+		err  error
+	)
+
+	if _, err = os.Stat(configFilePath); os.IsNotExist(err) {
+		return false, err
+	}
+
+	if file, err = os.ReadFile(configFilePath); err != nil {
+		return false, err
+	}
+	if err = json.Unmarshal(file, &data); err != nil {
+		return false, err
+	}
+	fmt.Printf("data: %v\n", data)
+
+	if _, exists := data.Auths[chart.RegistryURL]; exists {
+		return true, nil
+	} else {
+		return false, nil
+	}
 }
